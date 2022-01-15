1ab43457c6b2ed324ea4014933c0c2737536f4b6
kimhanjune93 <kimhanjune93@gmail.com>
2022-01-15 21:46:53 +0900

    feat: 숫자야구 만들기(1/2)

--- 3.숫자야구/NumberBaseball.jsx
+++ 3.숫자야구/NumberBaseball.jsx
@@ -1,25 +1,72 @@
 import React, { Component } from "react";
 import Try from "./Try";
 function getNumbers() {
-  const candidate = [1,2,3,4,5,6,7,8,9];
+  const candidate = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const array = [];
+  for (let i = 0; i < 4; i++) {
+    const chosen = candidate.splice(Math.floor(Math.random() * (9 - i)), 1)[0];
+    array.push(chosen);
+  }
+  return array;
 }
 
 class NumberBaseball extends Component {
   state = {
     result: "",
     value: "",
-    answer: getNumbers(),
+    answer: getNumbers(), // ex: [1,3,5,7]
     tries: [],
   };
   onSubmitForm = (e) => {
     e.preventDefault();
-    if (this.state.value === this.state.answer.join('')) {
-
+    if (this.state.value === this.state.answer.join("")) {
+      this.setState({
+        result: "홈런",
+        tries: [
+          ...this.state.tries,
+          { try: this.state.value, result: "홈런!" },
+        ], // push 쓰면 안됨.(불변성)
+      });
     } else {
-
+      const answerArray = this.state.value.split("").map((v) => parseInt(v));
+      let strike = 0;
+      let ball = 0;
+      if (this.state.tries.length >= 9) {
+        this.setState({
+          result: `10번 넘게 틀려서 실패! 답은 ${this.state.answer.join(
+            ","
+          )}였습니다!}`,
+        });
+        alert("게임을 다시 시작합니다.");
+        this.setState({
+          value: "",
+          answer: getNumbers(),
+          tries: [],
+        });
+      } else {
+        for (let i = 0; i < 4; i++) {
+          if (answerArray[i] === this.state.answer[i]) {
+            strike += 1;
+          } else if (this.state.answer.includes(answerArray[i])) {
+            ball += 1;
+          }
+        }
+        this.setState({
+          tries: [
+            ...this.state.tries,
+            {
+              try: this.state.value,
+              result: `${strike} 스트라이크, ${ball} 볼입니다.`,
+            },
+          ],
+          value: "",
+        });
+      }
     }
   };
-  onChangeInput = () => {};
+  onChangeInput = () => {
+    this.setState({ value: e.target.value });
+  };
 
   fruits = [
     { fruit: "사과", taste: "맛있다" },
@@ -39,9 +86,7 @@ class NumberBaseball extends Component {
         <div>시도: {this.state.tries.length}</div>
         <ul>
           {this.fruits.map((v, i) => {
-            return (
-              <Try value = {v} index = {i} key = {v.fruit + v.taste}/>
-            );
+            return <Try value={v} index={i} key={v.fruit + v.taste} />;
           })}
         </ul>
       </>
