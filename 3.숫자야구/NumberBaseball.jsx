f968e4051301968a8d446651b6f5a67973e3583c
kimhanjune93 <kimhanjune93@gmail.com>
2022-01-16 20:59:46 +0900

    feat: 숫자야구 완성

--- 3.숫자야구/NumberBaseball.jsx
+++ 3.숫자야구/NumberBaseball.jsx
@@ -1,9 +1,10 @@
-import React, { Component } from "react";
-import Try from "./Try";
-function getNumbers() {
-  const candidate = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+import React, { Component, createRef } from 'react';
+import Try from './Try';
+
+function getNumbers() { // 숫자 네 개를 겹치지 않고 랜덤하게 뽑는 함수
+  const candidate = [1,2,3,4,5,6,7,8,9];
   const array = [];
-  for (let i = 0; i < 4; i++) {
+  for (let i = 0; i < 4; i += 1) {
     const chosen = candidate.splice(Math.floor(Math.random() * (9 - i)), 1)[0];
     array.push(chosen);
   }
@@ -12,81 +13,86 @@ function getNumbers() {
 
 class NumberBaseball extends Component {
   state = {
-    result: "",
-    value: "",
+    result: '',
+    value: '',
     answer: getNumbers(), // ex: [1,3,5,7]
-    tries: [],
+    tries: [], // push 쓰면 안 돼요
   };
+
   onSubmitForm = (e) => {
+    const { value, tries, answer } = this.state;
     e.preventDefault();
-    if (this.state.value === this.state.answer.join("")) {
-      this.setState({
-        result: "홈런",
-        tries: [
-          ...this.state.tries,
-          { try: this.state.value, result: "홈런!" },
-        ], // push 쓰면 안됨.(불변성)
+    if (value === answer.join('')) {
+      this.setState((prevState) => {
+        return {
+          result: '홈런!',
+          tries: [...prevState.tries, { try: value, result: '홈런!' }],
+        }
       });
-    } else {
-      const answerArray = this.state.value.split("").map((v) => parseInt(v));
+      alert('게임을 다시 시작합니다!');
+      this.setState({
+        value: '',
+        answer: getNumbers(),
+        tries: [],
+      });
+      this.inputRef.current.focus();
+    } else { // 답 틀렸으면
+      const answerArray = value.split('').map((v) => parseInt(v));
       let strike = 0;
       let ball = 0;
-      if (this.state.tries.length >= 9) {
+      if (tries.length >= 9) { // 10번 이상 틀렸을 때
         this.setState({
-          result: `10번 넘게 틀려서 실패! 답은 ${this.state.answer.join(
-            ","
-          )}였습니다!}`,
+          result: `10번 넘게 틀려서 실패! 답은 ${answer.join(',')}였습니다!`,
         });
-        alert("게임을 다시 시작합니다.");
+        alert('게임을 다시 시작합니다!');
         this.setState({
-          value: "",
+          value: '',
           answer: getNumbers(),
           tries: [],
         });
+        this.inputRef.current.focus();
       } else {
-        for (let i = 0; i < 4; i++) {
-          if (answerArray[i] === this.state.answer[i]) {
+        for (let i = 0; i < 4; i += 1) {
+          if (answerArray[i] === answer[i]) {
             strike += 1;
-          } else if (this.state.answer.includes(answerArray[i])) {
+          } else if (answer.includes(answerArray[i])) {
             ball += 1;
           }
         }
-        this.setState({
-          tries: [
-            ...this.state.tries,
-            {
-              try: this.state.value,
-              result: `${strike} 스트라이크, ${ball} 볼입니다.`,
-            },
-          ],
-          value: "",
+        this.setState((prevState) => {
+          return {
+            tries: [...prevState.tries, { try: value, result: `${strike} 스트라이크, ${ball} 볼입니다`}],
+            value: '',
+          };
         });
+        this.inputRef.current.focus();
       }
     }
   };
-  onChangeInput = () => {
-    this.setState({ value: e.target.value });
+
+  onChangeInput = (e) => {
+    console.log(this.state.answer);
+    this.setState({
+      value: e.target.value,
+    });
   };
 
-  fruits = [
-    { fruit: "사과", taste: "맛있다" },
-    { fruit: "배", taste: "맛있다" },
-  ];
+  inputRef = createRef(); // this.inputRef
+
   render() {
+    const { result, value, tries } = this.state;
     return (
       <>
-        <h1>{this.state.result}</h1>
-        <form onSubmit={this.onSubmit}>
-          <input
-            maxLength={4}
-            value={this.state.value}
-            onChange={this.onChangeInput}
-          />
+        <h1>{result}</h1>
+        <form onSubmit={this.onSubmitForm}>
+          <input ref={this.inputRef} maxLength={4} value={value} onChange={this.onChangeInput} />
         </form>
-        <div>시도: {this.state.tries.length}</div>
+        <div>시도: {tries.length}</div>
         <ul>
-          {this.fruits.map((v, i) => {
-            return <Try value={v} index={i} key={v.fruit + v.taste} />;
+          {tries.map((v, i) => {
+            return (
+              <Try key={`${i + 1}차 시도 :`} tryInfo={v} />
+            );
           })}
         </ul>
       </>
@@ -94,4 +100,4 @@ class NumberBaseball extends Component {
   }
 }
 
-export default NumberBaseball;
+export default NumberBaseball; // import NumberBaseball;
