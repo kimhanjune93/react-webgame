50ca75760508d505bd3622155bc54d0752b47c77
kimhanjune93 <kimhanjune93@gmail.com>
2022-01-18 17:15:06 +0900

    feat: 함수형 컴포넌트 메모이제이션

--- lecture/RenderTest.jsx
+++ lecture/RenderTest.jsx
@@ -1,20 +1,35 @@
-import React, { PureComponent } from "react";
-class Test extends PureComponent {
-  state = {
-    array: [],
-  };
-  onClick = () => {
-    this.setState({
-      array:[...this.state.array,1],
+import React, { useState } from "react";
+
+const Test = () => {
+  const [array, setArray] = useState([]);
+
+  const onClick = () => {
+    setArray((prevArray) => {
+      return [...prevArray, 1];
     });
   };
-  render() {
-    console.log("렌더링", this.state);
-    return (
-      <div>
-        <button onClick={this.onClick}>클릭</button>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <button onClick={onClick}>클릭</button>
+    </div>
+  );
+};
+// class Test extends PureComponent {
+//   state = {
+//     array: [],
+//   };
+//   onClick = () => {
+//     this.setState({
+//       array:[...this.state.array,1],
+//     });
+//   };
+//   render() {
+//     console.log("렌더링", this.state);
+//     return (
+//       <div>
+//         <button onClick={this.onClick}>클릭</button>
+//       </div>
+//     );
+//   }
+// }
 export default Test;
