0c9effcbf0e8ef99e498cf62fe2df809c8470f11
kimhanjune93 <kimhanjune93@gmail.com>
2022-01-22 14:45:00 +0900

    feat: PureComponent

--- lecture/RSP.jsx
+++ lecture/RSP.jsx
@@ -1,6 +1,6 @@
-import React, {Component} from 'react';
+import React, {PureComponent} from 'react';
 
-class RSP extends Component {
+class RSP extends PureComponent {
     render() {
         return ();
     }
