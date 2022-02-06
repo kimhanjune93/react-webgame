92f4f9cb39d81c6ca41a75f7017a59b8620d66d2
kimhanjune93 <kimhanjune93@gmail.com>
2022-02-06 18:34:53 +0900

    feat: 로또게임 시작

---
+++ lecture/Lotto.jsx
@@ -0,0 +1,27 @@
+import React, { component } from "react";
+
+function getWinNumbers() {
+  console.log("getWinNumbers");
+  const candidate = Array(45)
+    .fill()
+    .map((v, i) => i + 1);
+  const shuffle = [];
+  while (candidate.length > 0) {
+    shuffle.push(
+      candidate.splice(Math.floor(Math.random() * candidate.length), 1)[0]
+    );
+  }
+  const bonusNumber = shuffle[fhuffle.length - 1];
+  const winNumbers = shuffle.slice(0, 6).sort((p, c) > p - c);
+  return [...winNumbers, bonusNumber];
+}
+
+class Lotto extends Component {
+  state = {
+    winNumbers: getWinNumbers(),
+    winBalls: [],
+    bonus: null,
+    redo: false,
+  };
+  render() {}
+}
