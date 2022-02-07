43c8e91679c3144ec0811ffc681c5c30aff54322
kimhanjune93 <kimhanjune93@gmail.com>
2022-02-07 20:13:33 +0900

    feat: 로또 추첨기 class형 컴포넌트 완성

--- lecture/Lotto.jsx
+++ lecture/Lotto.jsx
@@ -1,5 +1,5 @@
-import React, { component } from "react";
-
+import React, { Component } from "react";
+import Ball from "./Ball";
 function getWinNumbers() {
   console.log("getWinNumbers");
   const candidate = Array(45)
@@ -11,17 +11,60 @@ function getWinNumbers() {
       candidate.splice(Math.floor(Math.random() * candidate.length), 1)[0]
     );
   }
-  const bonusNumber = shuffle[fhuffle.length - 1];
-  const winNumbers = shuffle.slice(0, 6).sort((p, c) > p - c);
+  const bonusNumber = shuffle[shuffle.length - 1];
+  const winNumbers = shuffle.slice(0, 6).sort((p, c) => p - c);
   return [...winNumbers, bonusNumber];
 }
 
 class Lotto extends Component {
   state = {
-    winNumbers: getWinNumbers(),
+    winNumbers: getWinNumbers(), // 당첨 숫자들
     winBalls: [],
-    bonus: null,
+    bonus: null, // 보너스 공
     redo: false,
   };
-  render() {}
+
+  timeouts = [];
+
+  componentDidMount() {
+    const { winNumbers } = this.state;
+    for (let i = 0; i < this.state.winNumbers.length - 1; i++) {
+      this.timeouts[i] = setTimeout(() => {
+        this.setState((prevState) => {
+          return {
+            winBalls: [...prevState.winBalls, winNumbers[i]],
+          };
+        });
+      }, (i + 1) * 1000);
+    }
+    this.timeouts[6] = setTimeout(() => {
+      this.setState({
+        bonus: winNumbers[6],
+        redo: true,
+      });
+    }, 7000);
+  }
+  componentWillUnmount() {
+    // setTimeOut은 무조건 clear해줘야함.
+    this.timeouts.lforEach((v) => {
+      clearTimeout(v);
+    });
+  }
+  render() {
+    const { winBalls, bonus, redo } = this.state;
+    return (
+      <>
+        <div>당첨 숫자</div>
+        <div id="결과창">
+          {winBalls.map((v) => (
+            <Ball key={v} number={v} />
+          ))}
+        </div>
+        <div>보너스!</div>
+        {bonus && <Ball number={bonus} />}
+        <button onClick={redo ? this.onClickRedo : () => {}}>한 번 더!</button>
+      </>
+    );
+  }
 }
+export default Lotto;
