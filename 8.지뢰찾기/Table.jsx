810b5cedefd3c7f2a008ad417dd2641f1d57fbb1
kimhanjune93 <kimhanjune93@gmail.com>
2022-02-17 10:46:38 +0900

    feat: 지뢰찾기 react

---
+++ 8.지뢰찾기/Table.jsx
@@ -0,0 +1,19 @@
+import React, { useContext } from "react";
+import Tr from "./Tr";
+import { TableContext } from "./MineSearch";
+const Table = (props) => {
+  const { tableData } = useContext(TableContext);
+  return (
+    <table>
+      <tbody>
+        {Array(tableData.length)
+          .fill()
+          .map((tr, i) => (
+            <Tr key={i} rowIndex={i} />
+          ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default Table;
