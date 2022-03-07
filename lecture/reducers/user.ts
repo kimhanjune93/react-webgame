0ec78fdd0e9db00a513abaae495e9c6ca23dc2df
kimhanjune93 <kimhanjune93@gmail.com>
2022-03-08 00:03:20 +0900

    feat: reducer

--- lecture/reducers/user.ts
+++ lecture/reducers/user.ts
@@ -9,17 +9,33 @@ import {
   LogOutAction,
 } from "../actions/user";
 
-const initialState = {
+export interface UserState {
+  isLoggingIn: boolean;
+  data: {
+    nickname: string;
+  } | null;
+}
+
+const initialState: UserState = {
   isLoggingIn: false,
   data: null,
 };
-type UserReducerAction =
+type UserReducerActions =
   | LogInFailureAction
   | LogInRequestAction
   | LogOutAction
   | LogInSuccessAction;
-const userReducer = (prevState, action) => {
+const userReducer = (prevState = initialState, action: UserReducerActions) => {
   switch (action.type) {
+    case LOG_IN_REQUEST:
+    case LOG_IN_SUCCESS:
+    case LOG_IN_FAILURE:
+    case LOG_OUT:
+      return {
+        ...prevState,
+        data: null,
+      };
+
     default:
       return prevState;
   }
